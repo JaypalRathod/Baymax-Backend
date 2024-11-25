4a2c4fe98b26ee9c0263c98492e9b19e27e79472
Jaypal Rathod <jaypalrathod7147@gmail.com>
2024-11-25 11:27:26 +0530

    Baymax backend init

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const connectDB = require('./config/connect');
+const notificationRoutes = require('./routes/notifications');
+
+const app = express();
+app.use(express.json());
+
+app.use('/notifications', notificationRoutes)
+
+const start = async () => {
+    try {
+
+        await connectDB(process.env.MONGO_URI);
+        app.listen(process.env.PORT || 3000, () =>
+            console.log(`HTTP Server is running on port ${process.env.PORT || 3000} `)
+        );
+
+    } catch (error) {
+        console.log("Error starting server", error)
+    }
+};
+
+start();
